fd5eb100f2d03c70a0dcf0b0a9cafb668317265a
Thiago Miranda <thiago.miranda@byteon.com.br>
2024-07-19 17:03:27 -0300

    Intital

--- src/components/MainArea/MainArea.tsx
+++ src/components/MainArea/MainArea.tsx
@@ -19,7 +19,6 @@ export const MainArea = ({screens, idView}:MainAreaProps) => {
           screen.id === idView && screen.component
         ))
       }
-      <h1>OLAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
     </div>
   )
 }
