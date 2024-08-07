97eae67603b9ad3a18c200732ed3774c83bff665
Vinicius Fernandes <vinicius.fernandes@bravolog.com.br>
2024-08-07 14:32:46 -0300

    refactoring - drawer

--- src/Drawer/CustomDrawer.tsx
+++ src/Drawer/CustomDrawer.tsx
@@ -2,6 +2,7 @@ import { Button } from '@nextui-org/react'
 import React, { useState, CSSProperties, Dispatch } from 'react'
 import Drawer from 'react-modern-drawer'
 import 'react-modern-drawer/dist/index.css'
+import { CiCircleInfo } from "react-icons/ci";
 
 import {
   IoIosArrowUp ,
@@ -18,10 +19,16 @@ interface Props extends React.HTMLProps<HTMLDivElement> {
 const CustomDrawer: React.FC<Props> = ({ direction, ...props }) => {
   const [isMinimized, setIsMinimized] = useState(false)
   const isMobile = useMediaQuery('(max-width: 768px)')
+  const [isOpen, setIsOpen] = useState(true)
   const handleMinimize = () => {
     setIsMinimized(!isMinimized)
   }
-  
+  const handleClose = () => {
+    setIsOpen(false)
+  }
+  const handleOpen = () => {
+    setIsOpen(true) // Reabre o Drawer
+  }
   const getDrawerSize = () => {
     if (isMinimized) {
       return '50px'
@@ -31,7 +38,7 @@ const CustomDrawer: React.FC<Props> = ({ direction, ...props }) => {
     bottom: '.2rem',
     left: isMobile ? '0' : 'unset',  // Deixa o 'left' como 'unset' em telas maiores
     right: isMobile ? '0' : '2rem',  // Define 'right' para telas maiores para posicionar o drawer
-    width: isMobile ? '100%' : '25rem',  // Ocupa 100% da largura em dispositivos móveis
+    width: isMobile ? '100vw' : '20rem',  // Ocupa 100% da largura em dispositivos móveis
     position: 'fixed',
     boxSizing: 'border-box',
     overflowY: 'auto',
@@ -45,8 +52,12 @@ const CustomDrawer: React.FC<Props> = ({ direction, ...props }) => {
 
   return (
     <>
+
+      <Button onClick={handleOpen} disabled={isOpen || !props.lengthFiles} className="w-full md:w-1/2">
+        Arquivos
+      </Button>
       <Drawer
-        open={props.lengthFiles ? true : false}
+        open={isOpen && !!props.lengthFiles}
         className={props.className}
         enableOverlay={false}
         direction={direction}
@@ -57,7 +68,7 @@ const CustomDrawer: React.FC<Props> = ({ direction, ...props }) => {
           className="h-10 bg-gray-200 rounded-md text-black border-2 border-white sticky top-0 z-10 select-none"
         >
           <div className="h-full flex items-center justify-between">
-            <p className="pl-4">{props.lengthFiles} Arquivo(s) para upload.</p>
+            <p className="pl-4 text-sm">{props.lengthFiles} Arquivo(s) para upload.</p>
             <div className="flex items-center justify-center gap-6 mx-2">
               {isMinimized ? (
                 <IoIosArrowUp 
@@ -72,12 +83,11 @@ const CustomDrawer: React.FC<Props> = ({ direction, ...props }) => {
                   className="cursor-pointer hover:scale-110 transition-transform duration-200 rounded-full hover:bg-gray-300"
                 />
               )}
-              <div>
-                <IoMdClose
-                  size={26} 
-                  className="cursor-pointer rounded-full hover:scale-110 hover:bg-gray-300 transition-transform duration-200"
-                />
-              </div>
+              <IoMdClose
+                size={26}
+                onClick={handleClose}
+                className="cursor-pointer rounded-full hover:scale-110 hover:bg-gray-300 transition-transform duration-200"
+              />
             </div>
           </div>
         </div>
