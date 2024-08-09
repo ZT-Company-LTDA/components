a8624c2a51f78c392a960a87a535fdeec62d3d8e
Thiago Miranda <einsteinforcomputer@gmail.com>
2024-08-09 11:29:28 -0300

    refactor: alterando componentização

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,10 +1,10 @@
 'use client'
-import { NavBar } from "../SideBar/NavBar";
 import React from "react";
 import { MainArea } from "../MainArea/MainArea";
 import { ProviderScreen } from "../providers/ProviderScreen";
 import { ProviderTableCrud } from "../providers/ProviderTableCrud";
 import { useScreenContext } from "../contexts/ContextScreen";
+import { NavBar } from "../NavBar/NavBar";
 
 export const Dashboard = ({screens, aside, topbar, navBarType, mainAreaStyle, logo}:{screens: any, aside?:boolean, topbar?:boolean, navBarType: 'icon-bar' | 'intra-bar', mainAreaStyle:any, logo?:JSX.Element}) => {
   return (    
