9c7c8c354bb6a4f1a78cec8c409dd0b1ed4c69cd
Vinicius Fernandes <viniciusfernoli@gmail.com>
2024-08-13 13:59:41 -0300

    ajuste de tipagem

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,29 +5,30 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session extends NextAuthOptions {
-    user: {
-      id: number;
-      name: string;
-      email: string;
-      phone: string | null;
-      createdAt: string;
-      deletedAt: string | null;
-      updatedAt: string;
-      companyId: number;
-      company: {
-        id: number;
-        name: string;
-        cnpj: string;
-        address: string;
-        createdAt: string;
-        updatedAt: string;
-        deletedAt: string | null;
-      };
-      token: string;
-      sub: string;
-      iat: number;
-      exp: number;
-      jti: string;
-    }& DefaultSession["user"]
+    // user: {
+    //   id: number;
+    //   name: string;
+    //   email: string;
+    //   phone: string | null;
+    //   createdAt: string;
+    //   deletedAt: string | null;
+    //   updatedAt: string;
+    //   companyId: number;
+    //   company: {
+    //     id: number;
+    //     name: string;
+    //     cnpj: string;
+    //     address: string;
+    //     createdAt: string;
+    //     updatedAt: string;
+    //     deletedAt: string | null;
+    //   };
+    //   token: string;
+    //   sub: string;
+    //   iat: number;
+    //   exp: number;
+    //   jti: string;
+    // }& DefaultSession["user"]
+    user: any
   }
 }
