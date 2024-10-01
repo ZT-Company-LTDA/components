a878ac75db1293de7bb780509a5c65f1e24b89cd
Thiago Miranda <einsteinforcomputer@gmail.com>
2024-10-01 14:12:29 -0300

    feat: refactor table and modal

--- src/DinamicIcon/DinamicIcon.tsx
+++ src/DinamicIcon/DinamicIcon.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as FaIcons6 from 'react-icons/fa6';
-import * as MdIcons from 'react-icons/md';
-import * as IoIcons from 'react-icons/io';
-import * as FiIcons from 'react-icons/fi';
-import * as RioIcons from 'react-icons/ri';
-import * as Io5Icons from 'react-icons/io5';
-import * as HiIcons from 'react-icons/hi2';
-import * as CiIcons from 'react-icons/ci'; 
-import * as BsIcons from 'react-icons/bs'; 
-import * as CgIcons from 'react-icons/cg'; 
-import * as BiIcons from 'react-icons/bi'; 
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as FaIcons6 from "react-icons/fa6";
+import * as MdIcons from "react-icons/md";
+import * as IoIcons from "react-icons/io";
+import * as FiIcons from "react-icons/fi";
+import * as RioIcons from "react-icons/ri";
+import * as Io5Icons from "react-icons/io5";
+import * as HiIcons from "react-icons/hi2";
+import * as CiIcons from "react-icons/ci";
+import * as BsIcons from "react-icons/bs";
+import * as CgIcons from "react-icons/cg";
+import * as BiIcons from "react-icons/bi";
 
 type IconLibraries = {
-  [key: string]: { [key: string]: React.ComponentType<{ size?: number; color?: string; className?: string }> };
+  [key: string]: {
+    [key: string]: React.ComponentType<{
+      size?: number;
+      color?: string;
+      className?: string;
+    }>;
+  };
 };
 
 const iconLibraries: IconLibraries = {
@@ -21,9 +27,9 @@ const iconLibraries: IconLibraries = {
   fa6: FaIcons6,
   md: MdIcons,
   io: IoIcons,
-	fi: FiIcons,
-	ri: RioIcons,
-  io5: Io5Icons, 
+  fi: FiIcons,
+  ri: RioIcons,
+  io5: Io5Icons,
   hi2: HiIcons,
   ci: CiIcons,
   bs: BsIcons,
@@ -39,7 +45,13 @@ interface DynamicIconProps {
   className?: string;
 }
 
-export const DynamicIcon: React.FC<DynamicIconProps> = ({ library, iconName, size, color, className }) => {
+export const DynamicIcon: React.FC<DynamicIconProps> = ({
+  library,
+  iconName,
+  size,
+  color,
+  className,
+}) => {
   const IconLibrary = iconLibraries[library];
   if (!IconLibrary) {
     return <span>Library not found</span>;
@@ -49,7 +61,5 @@ export const DynamicIcon: React.FC<DynamicIconProps> = ({ library, iconName, siz
   if (!IconComponent) {
     return <span>Icon not found</span>;
   }
-
   return <IconComponent size={size} color={color} className={className} />;
 };
-
